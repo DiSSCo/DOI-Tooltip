ef43f663c462a7e54f934c9daf2ab7a62659027b
TomDijkema <tom.dijkema@naturalis.nl>
2024-04-29 11:40:33 +0200

    ImproveDOITooltip
    Improves the DOI Tooltip based upon the new (second) design iteration. Also improves functionality for fetching and displaying the tooltip.
    
    Applies to both the React and HTML versions

--- react/DOITooltip.tsx
+++ react/DOITooltip.tsx
@@ -2,13 +2,18 @@
 import { useState, useEffect, useRef } from "react";
 
 /* Import Styles */
-import './DOITooltip.css';
+import '../tooltip.css';
+
+/* Import Webroot */
+import TagLogo from '../webroot/tagLogo.png';
+import OrganisationLogo from '../webroot/building-columns-solid.svg';
+import DOILogo from '../webroot/DOI_logo.png';
 
 
 /* Props Typing */
 interface Props {
     doi: string
-    children: string | React.ReactElement,
+    children: string | React.ReactElement
 };
 
 
@@ -16,12 +21,11 @@ const DOITooltip = (props: Props) => {
     const { doi, children } = props;
 
     /* Hooks */
-    const targetRef = useRef<HTMLSpanElement>(null);
+    const targetRef = useRef<HTMLButtonElement>(null);
     const DOITooltipRef = useRef<HTMLDivElement>(null);
 
     /* Base variables */
-    const [record, setRecord] = useState();
-    const [active, setActive] = useState(false);
+    const [record, setRecord] = useState<any>({});
 
     /* Function to check if URL is valid */
     const IsValidUrl = (urlString: string) => {
@@ -33,24 +37,10 @@ const DOITooltip = (props: Props) => {
         }
     }
 
-    /* Function for fetching DOI details */
-    const TriggerTooltip = async () => {
-        try {
-            const response = await fetch(`https://dev.dissco.tech/handle-manager/api/v1/pids/${doi.replace('https://doi.org/', '')}`);
-            const record = await response.json();
-
-            if (record.data) {
-                setActive(true);
-
-                setRecord(record);
-            }
-        } catch (error) {
-            console.warn(error);
-        }
-    }
-
     /* Closing the Tooltip when clicked outside of it */
     const UseDOITooltip = () => {
+        const [active, setActive] = useState(false);
+
         useEffect(() => {
             const DOITooltipElement = DOITooltipRef.current as HTMLDivElement;
 
@@ -58,6 +48,8 @@ const DOITooltip = (props: Props) => {
                 if (!DOITooltipElement.contains(event.target)) {
                     if (active) {
                         setActive(false);
+
+                        setRecord({})
                     }
                 }
             }
@@ -66,70 +58,180 @@ const DOITooltip = (props: Props) => {
 
             return () => {
                 document.removeEventListener("mousedown", handleClickOutside);
+
             };
-        }, [DOITooltipRef, active])
+        }, [DOITooltipRef, active]);
+
+        return {
+            active,
+            setActive
+        };
     }
 
-    UseDOITooltip();
+    const DOITooltip = UseDOITooltip();
+
+    /* Function to map Handle or DOI response to valid record */
+    const FormatHandleNetResponse = (responseRecord: { [name: string]: any }) => {
+        const record = {
+            data: {
+                attributes: {
+                    referentName: responseRecord.values[16].data.value,
+                    specimenHost: responseRecord.values[18].data.value,
+                    specimenHostName: responseRecord.values[19].data.value,
+                    primarySpecimenObjectId: responseRecord.values[20].data.value,
+                    topicDiscipline: responseRecord.values[27].data.value,
+                    livingOrPreserved: responseRecord.values[29].data.value
+                }
+            }
+        };
+
+        return record;
+    }
+
+    /* Function to map Handle or DOI response to valid record */
+    const FormatDOIOrgResponse = (responseRecord: { [name: string]: any }) => {
+        const record = {
+            data: {
+                attributes: {
+                    referentName: responseRecord.values[26].data.value,
+                    specimenHost: responseRecord.values[14].data.value,
+                    specimenHostName: responseRecord.values[15].data.value,
+                    primarySpecimenObjectId: responseRecord.values[16].data.value,
+                    topicDiscipline: responseRecord.values[23].data.value,
+                    livingOrPreserved: responseRecord.values[25].data.value
+                }
+            }
+        };
+
+        return record;
+    }
+
+    /* Function for fetching DOI details */
+    const TriggerTooltip = async () => {
+        try {
+            /* If DOI does not contain local prefix, try to resolve with the general handler */
+            if (doi.includes('TEST') || doi.includes('SANDBOX')) {
+                let environment: string = doi.includes('SANDBOX') ? 'sandbox' : 'dev';
+
+                const response = await fetch(`https://${environment}.dissco.tech/handle-manager/api/v1/pids/${doi.replace(process.env.REACT_APP_DOI_URL as string, '')}`);
+                const record = await response.json();
+
+                if (record.data) {
+                    setRecord(record);
+                }
+            } else if (doi.includes('20.5000.1025')) {
+                const respone = await fetch(`https://hdl.handle.net/api/handles/${doi.replace(process.env.REACT_APP_DOI_URL as string, '')}`);
+                const responseRecord = await respone.json();
+
+                if (responseRecord.values.length) {
+                    const record = FormatHandleNetResponse(responseRecord);
+
+                    setRecord(record);
+                }
+            } else if (doi.includes('10.3535')) {
+                const response = await fetch(`https://doi.org/api/handles/${doi.replace(process.env.REACT_APP_DOI_URL as string, '')}`);
+                const responseRecord = await response.json();
+
+                if (responseRecord.values.length) {
+                    const record = FormatDOIOrgResponse(responseRecord);
+
+                    setRecord(record);
+                }
+            } else {
+                setRecord({});
+            }
+
+            DOITooltip.setActive(true);
+        } catch (error) {
+            console.warn(error);
+
+            DOITooltip.setActive(true);
+        }
+    }
 
     /* Set offset styles for DOI Tooltip */
     const offsetStyles = {
-        marginTop: targetRef.current ? `${targetRef.current?.offsetTop + 20}px` : '0px',
+        marginTop: (targetRef.current && DOITooltipRef.current && record.data) ? `${targetRef.current?.offsetTop - 210}px`
+            : (targetRef.current && DOITooltipRef.current) ? `${targetRef.current?.offsetTop - 45}px` : '0px',
         marginLeft: targetRef.current ? `${targetRef.current?.offsetLeft}px` : '0px'
     }
 
     return (
         <>
-            <span ref={targetRef} onClick={() => TriggerTooltip()}> {children} </span>
+            <button type="button" style={{ color: 'blue', cursor: 'pointer' }} ref={targetRef}
+                onMouseEnter={() => TriggerTooltip()}
+            >
+                {children}
+            </button>
 
-            <div id="disscoTooltip" className={`tooltip ${active && 'active'}`} ref={DOITooltipRef} style={offsetStyles}>
-                {/* Digital Extended Specimen */}
-                <div className="tooltipRow">
-                    <div className="widthLeft">
-                        <p className="digitalExtendedSpecimenTitle"> DES </p>
-                    </div>
-                    <div className="widthRight">
-                        <p id="tooltipScientificName" className="digitalExtendedSpecimenTitle">
-                            <a className="tooltipLink" href={`https://dev.dissco.tech/ds/${doi}`} target="blank">
-                                {record.data.attributes.referentName}
-                            </a>
-                        </p>
-                        <p id="tooltipStatus" className="preservedStatus textOverflow"> {`${record.data.attributes.topicDiscipline.toLowerCase()}
-                            ${record.data.attributes.livingOrPreserved} specimen`}
-                        </p>
-                    </div>
-                </div>
+            <div id="disscoTooltip" className={`tooltip ${DOITooltip.active && 'active'} ${!record.data && 'error'}`} ref={DOITooltipRef} style={offsetStyles}>
+                {record.data ?
+                    <>
+                        {/* Digital Extended Specimen */}
+                        <div className="tooltipRow grow">
+                            <div className="widthLeft">
+                                <img src={TagLogo}
+                                    alt="Tag Logo"
+                                    className="tagLogo"
+                                />
+                            </div>
+                            <div className="widthRight">
+                                <div>
+                                    <a className="tooltipLink" href={`https://dev.dissco.tech/ds/${doi}`} target="_blank">
+                                        <p id="tooltipScientificName" className="digitalExtendedSpecimenTitle"> {record.data.attributes.referentName} </p>
+                                    </a>
 
-                {/* ID */}
-                <div className="tooltipRow margin">
-                    <span className="widthLeft">
-                        <p className=""> ID </p>
-                    </span>
-                    <span className="widIDTitlethRight">
-                        {IsValidUrl(record.data.attributes.primarySpecimenObjectId) ?
-                            <a href={record.data.attributes.primarySpecimenObjectId}> {record.data.attributes.primarySpecimenObjectId} </a>
-                            : <p id="tooltipID" className="IDField"> {record.data.attributes.primarySpecimenObjectId} </p>
-                        }
-                        <p id="tooltipGUID" className="IDField"> (Catalog Record GUID) </p>
-                    </span>
-                </div>
+                                    <p className="verbatimName">(Verbatim name)</p>
+                                </div>
+                                <div className="preservedStatusDiv grow">
+                                    <p id="tooltipStatus" className="preservedStatus textOverflow">
+                                        {`${record.data.attributes.livingOrPreserved}
+                                    ${record.data.attributes.topicDiscipline.toLowerCase()} specimen`}
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
 
-                {/* Organisation */}
-                <div className="tooltipRow margin">
-                    <span className="widthLeft">
-                        <img src="../webroot/building-columns-solid.svg" className="organisationIcon" alt="ORG" />
-                    </span>
-                    <span className="widthRight">
-                        <a className="tooltipLink" href={record.data.attributes.specimenHost} target="_blank">
-                            <p id="tooltipOrganisation" className="organisationTitle"> {record.data.attributes.specimenHostName} </p>
-                        </a>
-                    </span>
-                </div>
+                        {/* ID */}
+                        <div className="tooltipRow">
+                            <span className="widthLeft">
+                                <p className="IDTitle"> ID </p>
+                            </span>
+                            <span className="widthRight">
+                                {IsValidUrl(record.data.attributes.primarySpecimenObjectId) ?
+                                    <a href={record.data.attributes.primarySpecimenObjectId}
+                                        className="tooltipLink"
+                                    >
+                                        {record.data.attributes.primarySpecimenObjectId}
+                                    </a>
+                                    : <p id="tooltipID" className="IDField">{record.data.attributes.primarySpecimenObjectId}</p>
+                                }
 
-                {/* DOI Logo */}
-                <div className="tooltopRow DOIRow">
-                    <img src="../webroot/DOI_logo.png" alt="DOI Logo" className="DOILogo" />
-                </div>
+                                <p id="tooltipGUID" className="catalogNumber"> (Catalog number) </p>
+                            </span>
+                        </div>
+
+                        {/* Organisation */}
+                        <div className="tooltipRow">
+                            <span className="widthLeft">
+                                <img src={OrganisationLogo} className="organisationIcon" alt="ORG" />
+                            </span>
+                            <span className="widthRight">
+                                <a className="tooltipLink" href={record.data.attributes.specimenHost} target="_blank">
+                                    <p id="tooltipOrganisation" className="organisationTitle"> {record.data.attributes.specimenHostName} </p>
+                                </a>
+                            </span>
+                        </div>
+
+                        {/* DOI Logo */}
+                        <div className="tooltopRow DOIRow">
+                            <span className="digitalExtendedSpecienNote">DES</span>
+
+                            <img src={DOILogo} alt="DOI Logo" className="DOILogo" />
+                        </div>
+                    </>
+                    : <p className="warningMessage"> Invalid DOI was provided, please try again </p>
+                }
             </div>
         </>
     );
